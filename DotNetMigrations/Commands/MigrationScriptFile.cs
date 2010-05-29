35803c484bd3294dcffbe4e52776f1603c54ae7b
Joshua Poehls <joshua@zduck.com>
2010-05-29 17:19:12 -0500

    fixed bug in MigrationScriptFile's CompareTo() method. added unit tests for the MigrationScriptFile class.

--- DotNetMigrations/Commands/MigrationScriptFile.cs
+++ DotNetMigrations/Commands/MigrationScriptFile.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace DotNetMigrations.Commands
 {
-    public class MigrationScriptFile : IComparable<MigrationScriptFile>
+    public class MigrationScriptFile : IComparable<MigrationScriptFile>, IEquatable<MigrationScriptFile>
     {
         public MigrationScriptFile(string filePath)
         {
@@ -13,20 +13,58 @@ namespace DotNetMigrations.Commands
             ParseVersion();
         }
 
+        public string FilePath { get; private set; }
+        public long Version { get; private set; }
+
+        #region IComparable<MigrationScriptFile> Members
+
+        public int CompareTo(MigrationScriptFile other)
+        {
+            return Version.CompareTo(other.Version);
+        }
+
+        #endregion
+
+        #region IEquatable<MigrationScriptFile> Members
+
+        public bool Equals(MigrationScriptFile other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.FilePath, FilePath);
+        }
+
+        #endregion
+
         private void ParseVersion()
         {
-            var sVersion = Path.GetFileName(FilePath).Split('_').FirstOrDefault();
+            string sVersion = Path.GetFileName(FilePath).Split('_').FirstOrDefault();
             long v;
             long.TryParse(sVersion, out v);
             Version = v;
         }
 
-        public string FilePath { get; private set; }
-        public long Version { get; private set; }
-
-        public int CompareTo(MigrationScriptFile other)
+        public override bool Equals(object obj)
         {
-            return Version.CompareTo(other);
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (MigrationScriptFile)) return false;
+            return Equals((MigrationScriptFile) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (FilePath != null ? FilePath.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(MigrationScriptFile left, MigrationScriptFile right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(MigrationScriptFile left, MigrationScriptFile right)
+        {
+            return !Equals(left, right);
         }
     }
 }
---
+++ DotNetMigrations_UnitTests/Commands/MigrationScriptFileUnitTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using DotNetMigrations.Commands;
+using NUnit.Framework;
+
+namespace DotNetMigrations.UnitTests.Commands
+{
+    [TestFixture]
+    public class MigrationScriptFileUnitTests
+    {
+        [Test]
+        public void CompareTo_should_find_two_with_the_same_version_equal()
+        {
+            //  arrange
+            const string path1 = "C:\\test\\123_my_migration_script.sql";
+            var script1 = new MigrationScriptFile(path1);
+
+            const string path2 = "C:\\test\\123_another_migration_script.sql";
+            var script2 = new MigrationScriptFile(path2);
+
+            //  act
+            int result = script1.CompareTo(script2);
+
+            //  assert
+            const int expectedResult = 0;
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Constructor_should_parse_path_correctly()
+        {
+            //  arrange
+            const string path = "C:\\test\\temp\\1234_my_migration_script.sql";
+
+            //  act
+            var script = new MigrationScriptFile(path);
+
+            //  assert
+            Assert.AreEqual(path, script.FilePath);
+        }
+
+        [Test]
+        public void Constructor_should_parse_version_when_present()
+        {
+            //  arrange
+            const string path = "C:\\test\\temp\\1234_my_migration_script.sql";
+
+            //  act
+            var script = new MigrationScriptFile(path);
+
+            //  assert
+            const long expectedVersion = 1234;
+            Assert.AreEqual(expectedVersion, script.Version);
+        }
+
+        [Test]
+        public void Constructor_should_set_version_to_default_when_not_found_in_path()
+        {
+            //  arrange
+            const string pathWithoutVersion = "C:\\test\\temp\\my_migration_script.sql";
+
+            //  act
+            var script = new MigrationScriptFile(pathWithoutVersion);
+
+            //  assert
+            const long expectedVersion = 0;
+            Assert.AreEqual(expectedVersion, script.Version);
+        }
+
+        [Test]
+        public void Equality_operator_should_find_two_with_the_same_FilePath_equal()
+        {
+            //  arrange
+            const string path1 = "C:\\test\\123_my_migration_script.sql";
+            var script1 = new MigrationScriptFile(path1);
+
+            const string path2 = "C:\\test\\123_my_migration_script.sql";
+            var script2 = new MigrationScriptFile(path2);
+
+            //  act
+            bool result = (script1 == script2);
+
+            //  assertS
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Equals_should_find_two_with_the_same_FilePath_equal()
+        {
+            //  arrange
+            const string path1 = "C:\\test\\123_my_migration_script.sql";
+            var script1 = new MigrationScriptFile(path1);
+
+            const string path2 = "C:\\test\\123_my_migration_script.sql";
+            var script2 = new MigrationScriptFile(path2);
+
+            //  act
+            bool result = script1.Equals(script2);
+
+            //  assertS
+            Assert.IsTrue(result);
+        }
+    }
+}
