d9fc9fe056cf8bd7c57190b04a128c3f572e9b43
Joshua Poehls <joshua@zduck.com>
2011-04-29 21:15:22 -0500

    Fixed bug with the seq_num versioning schema when generating the first migration script.

--- src/DotNetMigrations/Migrations/SequentialNumberVersion.cs
+++ src/DotNetMigrations/Migrations/SequentialNumberVersion.cs
@@ -1,16 +1,29 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DotNetMigrations.Migrations
 {
     public class SequentialNumberVersion : IVersionStrategy
     {
+        #region IVersionStrategy Members
+
         public long GetNewVersionNumber(IMigrationDirectory migrationDirectory)
         {
+            long lastNumber = 0;
+
             // get the latest number in use
-            var lastNumber = migrationDirectory.GetScripts().Max(script => script.Version);
+            IEnumerable<IMigrationScriptFile> scripts = migrationDirectory.GetScripts();
+
+            if (scripts.Count() > 0)
+            {
+                lastNumber = scripts.Max(script => script.Version);
+            }
 
             // increment and return
             return lastNumber + 1;
         }
+
+        #endregion
     }
 }
---
+++ src/DotNetMigrations_UnitTests/Migrations/SequentialNumberVersionUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DotNetMigrations.Core;
+using DotNetMigrations.Migrations;
+using DotNetMigrations.UnitTests.Mocks;
+using DotNetMigrations.UnitTests.Stubs;
+using NUnit.Framework;
+
+namespace DotNetMigrations.UnitTests.Migrations
+{
+    [TestFixture]
+    public class SequentialNumberVersionUnitTests
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void Setup()
+        {
+            _configManager = new InMemoryConfigurationManager();
+            _subject = new SequentialNumberVersion();
+
+        }
+
+        #endregion
+
+        private SequentialNumberVersion _subject;
+        private InMemoryConfigurationManager _configManager;
+
+        [Test]
+        public void GetNewVersionNumber_should_return_1_when_directory_is_empty()
+        {
+            // arrange
+            var migDir = new MigrationDirectory(_configManager);
+            
+            // act
+            var num = _subject.GetNewVersionNumber(migDir);
+
+            // assert
+            Assert.AreEqual(1, num);
+        }
+    }
+}
