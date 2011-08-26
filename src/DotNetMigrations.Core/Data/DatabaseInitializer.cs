6d4cdd93c078668f1725cb4fb1b8f78e4a88ca8c
Joshua Poehls <joshua@zduck.com>
2011-08-26 16:57:12 -0500

    Fixed issue #21 where migrations past version 9 would break due to incorrect sorting in the SQL that selects the MAX version number from the table. IMPORTANT: existing databases will need to have a manual ALTER run on them to change the [schema_migrations].[version] column from a [varchar] to [int] data type. This will NOT be done automatically.

--- src/DotNetMigrations.Core/Data/DatabaseInitializer.cs
+++ src/DotNetMigrations.Core/Data/DatabaseInitializer.cs
@@ -144,7 +144,7 @@ namespace DotNetMigrations.Core.Data
         /// </summary>
         private void CreateMigrationTable()
         {
-            const string createTableCommand = "CREATE TABLE [schema_migrations]([version] [nvarchar](14) NOT NULL PRIMARY KEY)";
+            const string createTableCommand = "CREATE TABLE [schema_migrations]([version] [int] NOT NULL PRIMARY KEY)";
             const string firstRecordCommand = "INSERT INTO [schema_migrations] ([version]) VALUES (0)";
 
             using (var tran = _dataAccess.BeginTransaction())
