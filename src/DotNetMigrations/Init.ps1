a371e4ab883bee1e9dac438019e0c5d6001c5b4c
Kieran Benton <me@kieranbenton.com>
2012-01-06 16:36:19 +0000

    Including NugetTest application, I've not found a way to mock installs at all yet. You need to rename the package ID to DotNetMigrationsLocal in DotNetMigrations.nuspec and add a local Nuget repository to \Debug before attempting an installation.

---
+++ src/DotNetMigrations/Init.ps1
@@ -0,0 +1,15 @@
+﻿param($installPath, $toolsPath, $package)
+
+#function Db($question) {
+#    $answers | Get-Random
+#}
+#
+#Register-TabExpansion 'Get-Answer' @{
+#    'question' = { 
+#        "Is this my lucky day?",
+#        "Will it rain tonight?",
+#        "Do I watch too much TV?"
+#    }
+#}
+#
+#Export-ModuleMember Db
